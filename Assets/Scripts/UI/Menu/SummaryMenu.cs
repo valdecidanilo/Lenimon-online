d7c35c7becad503a3b9675131a245b8a064c9b58
Leniton <lenitoncarneiro@gmail.com>
2025-04-29 19:01:57 -0300

    summary inputs

--- Assets/Scripts/UI/Menu/SummaryMenu.cs
+++ Assets/Scripts/UI/Menu/SummaryMenu.cs
@@ -1,12 +1,34 @@
+using LenixSO.Logger;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Logger = LenixSO.Logger.Logger;
 
 public class SummaryMenu : ContextMenu<Pokemon>
 {
+    private InputAction navigateAction;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        navigateAction = InputSystem.actions.FindAction("UI/Navigate");
+    }
+
     public override void OpenMenu(Pokemon data)
     {
         gameObject.SetActive(true);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if(navigateAction.WasPressedThisFrame()) ChangeWindow(navigateAction.ReadValue<Vector2>());
+    }
+
+    private void ChangeWindow(Vector2 direction)
+    {
+        Logger.Log($"{direction}", LogFlags.Game);
     }
 
     public override void CloseMenu()
