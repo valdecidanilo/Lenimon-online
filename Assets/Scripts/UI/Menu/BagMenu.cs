2859b0055a10a804d263d622f9465338cb046f5a
Leniton <lenitoncarneiro@gmail.com>
2025-05-10 16:54:47 -0300

    base bag scripts

---
+++ Assets/Scripts/UI/Menu/BagMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagMenu : ContextMenu<Bag>
+{
+    public override void OpenMenu(Bag data)
+    {
+
+    }
+
+    public override void CloseMenu()
+    {
+
+    }
+}
