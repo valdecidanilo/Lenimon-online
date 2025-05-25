de3edea6c64f2a6fda6f535c03993516332af220
Leniton <lenitoncarneiro@gmail.com>
2025-05-25 16:56:55 -0300

    feat: custom effect add effect

--- Assets/Scripts/Battle/Effects/CustomEffect.cs
+++ Assets/Scripts/Battle/Effects/CustomEffect.cs
@@ -6,5 +6,6 @@ public class CustomEffect : Effect
 {
     private CoroutineAction<BattleEvent> effect = new();
     public CustomEffect(Func<BattleEvent,IEnumerator> customEffect) { effect.RegisterCallback(customEffect); }
+    public void AddEffect(Func<BattleEvent, IEnumerator> customEffect) => effect.RegisterCallback(customEffect);
     public override IEnumerator EffectSequence(BattleEvent evt) { yield return effect?.Invoke(evt); }
 }
