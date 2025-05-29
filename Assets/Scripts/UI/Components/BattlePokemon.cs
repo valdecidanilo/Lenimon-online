7d9eba8a3d12a7d9a2c29a8b1467b2fad4993aeb
Leniton <leniton.carneiro@gdstecnologia.com.br>
2025-05-29 18:10:14 -0300

    feat: battle pokemon*
    *no functionality

---
+++ Assets/Scripts/UI/Components/BattlePokemon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattlePokemon : MonoBehaviour
+{
+    [SerializeField] private Image pokemonImage;
+    [SerializeField] private Image pokemonOverlay;
+}
