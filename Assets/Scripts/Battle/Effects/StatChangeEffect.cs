54cd9426633e5f19a40342b68949d08b51c90d60
Leniton <lenitoncarneiro@gmail.com>
2025-06-10 20:37:14 -0300

    feat: multi stat change

--- Assets/Scripts/Battle/Effects/StatChangeEffect.cs
+++ Assets/Scripts/Battle/Effects/StatChangeEffect.cs
@@ -9,38 +9,97 @@ namespace Battle
 {
     public class StatChangeEffect : Effect
     {
-        private List<StatChange> changeRef;
-        private readonly StatType[] types;
-        private readonly int[] changes;
+        private readonly List<StatChange> changeRef;
+
+        private readonly int mainSign;
+
+        private readonly List<StatType> mainStats;
+        private readonly List<int> mainChanges;
+        private readonly string mainChangeText;
+
+        private readonly List<StatType> altStats;
+        private readonly List<int> altChanges;
+        private readonly string altChangeText;
         
         public StatChangeEffect(List<StatChange> statChanges)
         {
             changeRef = statChanges;
-            types = new StatType[statChanges.Count];
-            changes = new int[statChanges.Count];
-            for (int i = 0; i < types.Length; i++)
+
+            mainStats = new();
+            mainChanges = new();
+            altStats = new();
+            altChanges = new();
+
+            mainSign = (int)Mathf.Sign(changeRef[0].change);
+            StringBuilder mainText = new();
+            StringBuilder altText = new();
+            for (int i = 0; i < statChanges.Count; i++)
             {
-                types[i] = ApiToStat(statChanges[i].stat);
-                changes[i] = statChanges[i].change;
+                bool mainChange = (int)Mathf.Sign(changeRef[i].change) == mainSign;
+                StatType statType = ApiToStat(changeRef[i].stat);
+                int change = changeRef[i].change;
+                if (mainChange)
+                {
+                    //if (mainChanges.Count > 0)
+                    //{
+                    //    //change "and" for ","
+                    //    mainText.Remove(mainText.Length - 5, 5);
+                    //    mainText.Append(", ");
+                    //}
+                    mainText.Append($"{changeRef[i].stat.name.Replace("-", " ")}");
+                    mainText.Append($" and ");
+                    mainStats.Add(statType);
+                    mainChanges.Add(change);
+                }
+                else
+                {
+                    //if (altChanges.Count > 0)
+                    //{
+                    //    //change "and" for ","
+                    //    altText.Remove(altText.Length - 5, 5);
+                    //    altText.Append(", ");
+                    //}
+                    altText.Append($"{changeRef[i].stat.name.Replace("-", " ")}");
+                    altText.Append($" and ");
+                    altStats.Add(statType);
+                    altChanges.Add(change);
+                }
             }
+            mainText.Remove(mainText.Length - 5, 5);
+            mainText.Append(mainSign > 0 ? " rose" : " fell");
+            mainChangeText = mainText.ToString();
+            //Logger.Log(mainChangeText);
+            if (altChanges.Count <= 0) return;
+            altText.Remove(altText.Length - 5, 5);
+            altText.Append(mainSign < 0 ? " rose" : " fell");
+            altChangeText = altText.ToString();
+            //Logger.Log(altChangeText);
         }
         
         public override IEnumerator EffectSequence(BattleEvent evt)
         {
             StringBuilder sb = new($"{evt.target.name}'s ");
-            string changeType = changes[0] > 0 ? "rose" : "fell";
             Stats stats = evt.target.battleStats;
-            for (int i = 0; i < changes.Length; i++)
+            for (int i = 0; i < mainChanges.Count; i++)
             {
-                stats[types[i]] = Mathf.Clamp(stats[types[i]] + changes[i], -6, 6);
-                if(stats[types[i]] == evt.target.battleStats[types[i]]) sb.Append("");//can't rise
-                sb.Append($"{changeRef[i].stat.name.Replace("-", " ")}");
-                if (i < changes.Length - 1) sb.Append(" and ");
+                stats[mainStats[i]] = Mathf.Clamp(stats[mainStats[i]] + mainChanges[i], -6, 6);
             }
-            sb.Append($" {changeType}!");
+            sb.Append($"{mainChangeText}!");
             Logger.Log(sb.ToString(), LogFlags.Game);
-            yield return FightMenu.StatusChangeEffect(evt.target, changes[0] > 0);
-            yield return Announcer.AnnounceCoroutine(sb.ToString(), holdTime: 2f);
+            yield return FightMenu.StatusChangeEffect(evt.target, mainSign > 0);
+            yield return Announcer.AnnounceCoroutine(sb.ToString(), holdTime: 1.5f);
+
+            if (altChanges.Count <= 0) yield break;
+
+            sb = new($"{evt.target.name}'s ");
+            for (int i = 0; i < altChanges.Count; i++)
+            {
+                stats[altStats[i]] = Mathf.Clamp(stats[altStats[i]] + altChanges[i], -6, 6);
+            }
+            sb.Append($"{altChangeText}!");
+            Logger.Log(sb.ToString(), LogFlags.Game);
+            yield return FightMenu.StatusChangeEffect(evt.target, mainSign < 0);
+            yield return Announcer.AnnounceCoroutine(sb.ToString(), holdTime: 1.5f);
         }
 
         private static StatType ApiToStat(ApiReference reference)
