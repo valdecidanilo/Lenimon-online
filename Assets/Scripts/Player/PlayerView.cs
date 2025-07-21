505e57497f2ffd21ce76ce7f6373f89f563e7a40
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-07-21 09:00:54 -0300

    feat: adde new audios game

---
+++ Assets/Scripts/Player/PlayerView.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text nameText;
+        [SerializeField] private PlayerAnimatorController animatorController;
+
+        public void UpdateNickname(string nickname, bool inBattle)
+        {
+            if (nameText == null) return;
+            var displayName = inBattle ? $"<size=150%><sprite=12></size>\n{nickname}" : nickname;
+            nameText.SetText(displayName);
+        }
+
+        public void SetMoveDirection(Vector2 dir, bool isMoving)
+        {
+            animatorController.SetMoveDirection(dir, isMoving);
+        }
+
+        public void SetRunState(bool isRunning)
+        {
+            animatorController.SetRunState(isRunning);
+        }
+
+        public void SetIdleState(bool idle)
+        {
+            animatorController.SetIdleState(idle);
+        }
+
+        public void FlipSprite(Vector2 direction)
+        {
+            if (direction.x != 0)
+                animatorController.spriteRenderer.flipX = direction.x < 0;
+        }
+    }
+}
